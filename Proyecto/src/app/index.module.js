7de7cf2b15ce576bda6539f9d49a8196abb7dd2c
Yasiell Vallejos <yvallejos@penixlabs.com>
2016-05-27 16:19:14 -0600

    Se crea el proyecto de Yeoman con el .gitignore

---
+++ Proyecto/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('proyecto', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
